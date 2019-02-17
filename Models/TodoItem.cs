3eaa01ed45aa558892565344d7568924f59b7c1e
naruto <blackfoxgamingstudio@gmail.com>
2019-02-17 12:05:15 -0800

    Add files via upload

---
+++ Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Subject {get; set; }
+
+        public string Name { get; set; }
+
+        public string Description {get; set; }
+       
+        public bool IsComplete { get; set; }
+
+        public string Status {get; set; }
+         public string StartTime {get; set; }
+        public string CreationDate {get; set; }
+    }
+}
