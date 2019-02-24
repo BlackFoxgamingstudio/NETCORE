7321b53436f90092ab7d2c3593d6238c09640085
blackfox <blackfoxgamingstudio@gmail.com>
2019-02-24 15:43:04 -0800

    fix bug

--- wwwroot/app/scripts/app.js
+++ wwwroot/app/scripts/app.js
@@ -11,10 +11,7 @@ angular.module('todoApp', ['ngRoute'])
     $routeProvider.when("/Home", {
         controller: "todoListCtrl",
         templateUrl: "/App/Views/TodoList.html",
-        
-    $routeProvider.when("/Home", {
-        controller: "todoListCtrl",
-        templateUrl: "/App/Views/TodoList.html",
+   
 
     }).otherwise({ redirectTo: "/Home" });
 
