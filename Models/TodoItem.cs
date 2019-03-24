d9eccf1de5d2f1db02a62b15392e5345c868b3d8
naruto <blackfoxgamingstudio@gmail.com>
2019-03-23 17:50:01 -0700

    Add files via upload

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,16 +3,28 @@
     public class TodoItem
     {
         public long Id { get; set; }
-        public string Subject {get; set; }
+        public string account {get; set; }
 
-        public string Name { get; set; }
+        public string current_activity { get; set; }
 
-        public string Description {get; set; }
-       
+        public string current_task {get; set; }
+
+        public string contact {get; set; }
+        public string role {get; set; }
+        
+        public string usecase {get; set; }
+
+        public string note {get; set; }
+
+        public string action_plan {get; set; }
+
+         public string Email_template {get; set; }
+
+        public string user {get; set; }
+
+        public string creationdate {get; set; }
+        public string startdate { get; set; }
+        public string enddate { get; set; }
         public bool IsComplete { get; set; }
-
-        public string Status {get; set; }
-         public string StartTime {get; set; }
-        public string CreationDate {get; set; }
     }
 }
